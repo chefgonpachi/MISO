031e04ed0c2d987898c6b335f046d419e2688a98
chefgonpachi <gonpachi@protonmail.com>
2021-04-21 10:52:06 +0200

    Updated documents file

--- contracts/Utils/Documents.sol
+++ contracts/Utils/Documents.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 
 /**
@@ -7,38 +8,38 @@ pragma solidity 0.6.12;
 contract Documents {
 
     struct Document {
-        bytes32 docHash; // Hash of the document
-        uint256 lastModified; // Timestamp at which document details was last modified
-        string uri; // URI of the document that exist off-chain
+        uint32 docIndex;    // Store the document name indexes
+        uint64 lastModified; // Timestamp at which document details was last modified
+        string data; // data of the document that exist off-chain
     }
 
     // mapping to store the documents details in the document
-    mapping(bytes32 => Document) internal _documents;
+    mapping(string => Document) internal _documents;
     // mapping to store the document name indexes
-    mapping(bytes32 => uint256) internal _docIndexes;
+    mapping(string => uint32) internal _docIndexes;
     // Array use to store all the document name present in the contracts
-    bytes32[] _docNames;
+    string[] _docNames;
 
     // Document Events
-    event DocumentRemoved(bytes32 indexed _name, string _uri, bytes32 _documentHash);
-    event DocumentUpdated(bytes32 indexed _name, string _uri, bytes32 _documentHash);
+    event DocumentRemoved(string indexed _name, string _data);
+    event DocumentUpdated(string indexed _name, string _data);
 
     /**
-     * @notice Used to attach a new document to the contract, or update the URI or hash of an existing attached document
+     * @notice Used to attach a new document to the contract, or update the data or hash of an existing attached document
      * @dev Can only be executed by the owner of the contract.
      * @param _name Name of the document. It should be unique always
-     * @param _uri Off-chain uri of the document from where it is accessible to investors/advisors to read.
-     * @param _documentHash hash (of the contents) of the document.
+     * @param _data Off-chain data of the document from where it is accessible to investors/advisors to read.
      */
-    function _setDocument(bytes32 _name, string calldata _uri, bytes32 _documentHash) internal {
-        require(_name != bytes32(0), "Zero value is not allowed");
-        require(bytes(_uri).length > 0, "Should not be a empty uri");
-        if (_documents[_name].lastModified == uint256(0)) {
+    function _setDocument(string calldata _name, string calldata _data) internal {
+        require(bytes(_name).length > 0, "Zero name is not allowed");
+        require(bytes(_data).length > 0, "Should not be a empty data");
+        // Document storage document = _documents[_name];
+        if (_documents[_name].lastModified == uint64(0)) {
             _docNames.push(_name);
-            _docIndexes[_name] = _docNames.length;
+            _documents[_name].docIndex = uint32(_docNames.length);
         }
-        _documents[_name] = Document(_documentHash, now, _uri);
-        emit DocumentUpdated(_name, _uri, _documentHash);
+        _documents[_name] = Document(_documents[_name].docIndex, uint64(now), _data);
+        emit DocumentUpdated(_name, _data);
     }
 
     /**
@@ -47,39 +48,53 @@ contract Documents {
      * @param _name Name of the document. It should be unique always
      */
 
-    function _removeDocument(bytes32 _name) internal {
-        require(_documents[_name].lastModified != uint256(0), "ERC1643: Document should exist");
-        uint256 index = _docIndexes[_name] - 1;
+    function _removeDocument(string calldata _name) internal {
+        require(_documents[_name].lastModified != uint64(0), "Document should exist");
+        uint32 index = _documents[_name].docIndex - 1;
         if (index != _docNames.length - 1) {
             _docNames[index] = _docNames[_docNames.length - 1];
-            _docIndexes[_docNames[index]] = index + 1; 
+            _documents[_docNames[index]].docIndex = index + 1; 
         }
         _docNames.pop();
-        emit DocumentRemoved(_name, _documents[_name].uri, _documents[_name].docHash);
+        emit DocumentRemoved(_name, _documents[_name].data);
         delete _documents[_name];
     }
 
     /**
-     * @notice Used to return the details of a document with a known name (`bytes32`).
+     * @notice Used to return the details of a document with a known name (`string`).
      * @param _name Name of the document
-     * @return string The URI associated with the document.
-     * @return bytes32 The hash (of the contents) of the document.
+     * @return string The data associated with the document.
      * @return uint256 the timestamp at which the document was last modified.
      */
-    function getDocument(bytes32 _name) external view returns (string memory, bytes32, uint256) {
+    function getDocument(string calldata _name) external view returns (string memory, uint256) {
         return (
-            _documents[_name].uri,
-            _documents[_name].docHash,
-            _documents[_name].lastModified
+            _documents[_name].data,
+            uint256(_documents[_name].lastModified)
         );
     }
 
     /**
      * @notice Used to retrieve a full list of documents attached to the smart contract.
-     * @return bytes32 List of all documents names present in the contract.
+     * @return string List of all documents names present in the contract.
      */
-    function getAllDocuments() external view returns (bytes32[] memory) {
+    function getAllDocuments() external view returns (string[] memory) {
         return _docNames;
+    }
+
+    /**
+     * @notice Used to retrieve the total documents in the smart contract.
+     * @return uint256 Count of the document names present in the contract.
+     */
+    function getDocumentCount() external view returns (uint256) {
+        return _docNames.length;
+    }
+
+    /**
+     * @notice Used to retrieve the document name from index in the smart contract.
+     * @return string Name of the document name.
+     */
+    function getDocumentName(uint256 _index) external view returns (string memory) {
+        return _docNames[_index];
     }
 
 }
